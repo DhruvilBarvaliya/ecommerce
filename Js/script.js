342f6a67b931f1d2b9681d6ce90294633b8ba088
DhruvilBarvaliya <dhruvilbarvaliya20@gmail.com>
2024-08-30 18:59:22 +0530

    ecommerce website

---
+++ Js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("#Slider").owlCarousel({
+        items:1,
+        nav:true,
+        dots:false,
+        // navText:['<','>']
+    });
+  });
+
+
+
+  $(document).ready(function(){
+    $("#sec2").owlCarousel({
+        items:4,
+    });
+  });
